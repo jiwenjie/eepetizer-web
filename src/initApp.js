39c2c944e65b743761fd34556a1526df3a4f3ca4
吉文杰 <Jiwenjie97@gmail.com>
2021-04-12 16:46:37 +0800

    Create initApp.js

---
+++ src/initApp.js
@@ -0,0 +1,24 @@
+
+import App from './App.vue';    // 导入主页 vue 文件
+// 引入路由文件
+import router from './router/index';
+// 引入 store 文件
+import store from '@/store'
+
+// 全量引入 elementPlus
+import installElementPlus from './plugins/element'
+
+export default async function initAppView(createApp) {
+    try {
+        const { userInfo } = await store.dispatch('setUserInfo')
+        console.log('initApp', userInfo);
+        const app = createApp(App)
+        installElementPlus(app)
+        app.use(router)
+            .use(store)
+            .mount('#app')
+    } catch (error) {
+        console.error('init error:', error)
+        // errorLoadPage(Vue, ErrorPage)
+    }
+}
